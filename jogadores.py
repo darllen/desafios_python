17aa68d716700847b2461def9a42ad0fe466e878
Jamilly Anunciada <jamillymilly10@hotmail.com>
2022-10-28 13:47:45 -0300

    Update jogadores.py

--- jogadores.py
+++ jogadores.py
@@ -1,5 +1,9 @@
-""" Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato. 
-Aprimore a questão para que ele funciona com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+""" Crie um programa que gerencie o aproveitamento de um jogador de futebol. 
+O programa vai ler o nome do jogador e quantas partidas ele jogou. 
+Depois vai ler a quantidade de gols feitos em cada partida. No final, 
+tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato. 
+Aprimore a questão para que ele funciona com vários jogadores, incluindo um sistema de 
+visualização de detalhes do aproveitamento de cada jogador.
 """
 jogadores = []
 
